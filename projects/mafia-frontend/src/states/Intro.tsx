309e7ae1b9c1810b3d47fde6d2738477e13f1ec7
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2025-04-19 02:54:00 +0200

    chore: force git to take into consideration casing

---
+++ projects/mafia-frontend/src/states/Intro.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable no-console */
+import { AlgorandClient } from '@algorandfoundation/algokit-utils'
+import { useWallet } from '@txnlab/use-wallet-react'
+import { useSnackbar } from 'notistack'
+import React, { useState } from 'react'
+import { TownHallClient, TownHallFactory } from '../contracts/TownHall'
+import { GameState } from '../interfaces/gameState'
+import { getAlgodConfigFromViteEnvironment, getIndexerConfigFromViteEnvironment } from '../utils/network/getAlgoClientConfigs'
+
+interface IntroProps {
+  setGameState: React.Dispatch<React.SetStateAction<GameState>>
+  setAppId: React.Dispatch<React.SetStateAction<bigint>>
+}
+
+const Intro: React.FC<IntroProps> = ({ setGameState, setAppId }) => {
+  const [inputAppId, setInputAppId] = useState<string>('')
+
+  const { transactionSigner, activeAddress } = useWallet()
+
+  const { enqueueSnackbar } = useSnackbar()
+
+  const algodConfig = getAlgodConfigFromViteEnvironment()
+  const indexerConfig = getIndexerConfigFromViteEnvironment()
+  const algorand = AlgorandClient.fromConfig({
+    algodConfig,
+    indexerConfig,
+  })
+
+  algorand.setDefaultSigner(transactionSigner)
+
+  const handleProceed = async () => {
+    if (inputAppId.trim()) {
+      try {
+        // If app doesn't exist, this will throw an error:
+        await (
+          await algorand.client.getTypedAppClientById(TownHallClient, {
+            appId: BigInt(inputAppId),
+          })
+        ).state.global.player1AlgoAddr()
+
+        setAppId(BigInt(inputAppId))
+        setGameState(GameState.JoinGameLobby)
+      } catch (error) {
+        console.error('Error proceeding to game:', error)
+        alert('Failed to proceed. Please ensure the App ID is valid and try again.')
+      }
+    } else {
+      alert('Please provide a valid App ID.')
+    }
+  }
+
+  const handleCreateNewGame = async () => {
+    console.log('Creating a new game...')
+
+    const factory = new TownHallFactory({
+      defaultSender: activeAddress ?? undefined,
+      algorand,
+    })
+
+    const deployResult = await factory.send.create.createApplication().catch((e: Error) => {
+      enqueueSnackbar(`Error deploying the contract: ${e.message}`, { variant: 'error' })
+      return undefined
+    })
+
+    if (!deployResult) {
+      return
+    }
+
+    const { appClient } = deployResult
+
+    setAppId(appClient.appId)
+  }
+
+  return (
+    <div className="max-w-md">
+      <h1 className="text-4xl font-bold">Welcome, Resistance Fighters.</h1>
+      <p className="py-2">
+        The Resistance is on the brink of collapse. A spy lurks among you, sabotaging your efforts from the shadows. Time is running out.
+      </p>
+      <p className="py-2">
+        Armed with cryptography and blockchain technology, you’ve stayed ahead of your enemies—until now. Can you unmask the traitor before
+        it’s too late?
+      </p>
+      <p className="py-2">Enter an App ID to join an existing game or create a new one.</p>
+      <div className="grid gap-4">
+        <input
+          type="text"
+          className="input input-bordered w-full"
+          placeholder="Enter App ID"
+          value={inputAppId}
+          onChange={(e) => setInputAppId(e.target.value)}
+        />
+        <button className="btn m-2" onClick={handleProceed}>
+          Proceed to game
+        </button>
+        <button className="btn m-2" onClick={handleCreateNewGame}>
+          Create a New Game
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
---
+++ projects/mafia-frontend/src/states/JoinGameLobby.tsx
@@ -0,0 +1,131 @@
+/* eslint-disable no-console */
+import { AlgorandClient } from '@algorandfoundation/algokit-utils'
+import { useWallet } from '@txnlab/use-wallet-react'
+import * as algoring from 'algoring-ts'
+import React from 'react'
+import PlayerPickPanel from '../components/PlayerPickPanel'
+import usePlayersState from '../hooks/usePlayerState'
+import { Player } from '../interfaces/player'
+import { BLS12381G1_LENGTH, RING_SIG_NONCE_LENGTH } from '../utils/constants'
+import { getAlgodConfigFromViteEnvironment, getIndexerConfigFromViteEnvironment } from '../utils/network/getAlgoClientConfigs'
+
+interface JoinGameLobbyProps {
+  playerObject: Player
+}
+
+const JoinGameLobby: React.FC<JoinGameLobbyProps> = ({ playerObject }) => {
+  const { activeAddress, transactionSigner } = useWallet()
+
+  const { players } = usePlayersState(playerObject)
+
+  console.log(`players in joinGameLobby:${players}`)
+
+  const handleJoinGame = async () => {
+    const algodConfig = getAlgodConfigFromViteEnvironment()
+    const indexerConfig = getIndexerConfigFromViteEnvironment()
+    const algorand = AlgorandClient.fromConfig({
+      algodConfig,
+      indexerConfig,
+    })
+
+    algorand.setDefaultSigner(transactionSigner)
+
+    console.log('The players are currently:', players)
+
+    if (players.includes(playerObject.day_algo_address.addr.toString())) {
+      console.log('Already joined the game')
+      return
+    }
+
+    // Fund the contract so we can simulate state from it
+    const fundTownHallContractResults = await algorand.send.payment({
+      sender: activeAddress!,
+      signer: transactionSigner,
+      receiver: playerObject.day_client.appClient.appAddress,
+      amount: (144100).microAlgos(),
+    })
+
+    console.log('Funding Contract Results:', fundTownHallContractResults)
+
+    // Fund the day_algo_player address with 2 Algo
+    const fundDayPlayerAlgoResults = await algorand.send.payment({
+      sender: activeAddress!,
+      signer: transactionSigner,
+      receiver: playerObject.day_algo_address.addr,
+      amount: (2).algos(),
+    })
+
+    console.log('Funding Day Player Results:', fundDayPlayerAlgoResults)
+
+    const proof = algoring.NIZK_DLOG_generate_proof(playerObject.bls_private_key)
+
+    const NIZK_DLOG = new Uint8Array(3 * BLS12381G1_LENGTH + RING_SIG_NONCE_LENGTH)
+    NIZK_DLOG.set(proof[0])
+    NIZK_DLOG.set(proof[1], BLS12381G1_LENGTH)
+    NIZK_DLOG.set(proof[2], 2 * BLS12381G1_LENGTH)
+    NIZK_DLOG.set(proof[3], 3 * BLS12381G1_LENGTH)
+
+    const joinGameResult = await playerObject.day_client
+      .newGroup()
+      .joinGameLobby({
+        args: {
+          nizkDlog: NIZK_DLOG,
+        },
+      })
+      .dummyOpUp({
+        args: { i: 1 },
+      })
+      .dummyOpUp({
+        args: { i: 2 },
+      })
+      .dummyOpUp({
+        args: { i: 3 },
+      })
+      .dummyOpUp({
+        args: { i: 4 },
+      })
+      .dummyOpUp({
+        args: { i: 5 },
+      })
+      .dummyOpUp({
+        args: { i: 6 },
+      })
+      .dummyOpUp({
+        args: { i: 7 },
+      })
+      .dummyOpUp({
+        args: { i: 8 },
+      })
+      .dummyOpUp({
+        args: { i: 9 },
+      })
+      .send()
+
+    console.log('Join Game Result:', joinGameResult)
+
+    // Fetch players immediately after joining the game
+  }
+
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold">Game Lobby</h1>
+      <p className="py-4">Waiting for players to join...</p>
+
+      {players.length > 0 ? (
+        <PlayerPickPanel players={players} currentPlayerAddress={playerObject.day_algo_address.addr.toString()} />
+      ) : (
+        <p>No players in the lobby yet.</p>
+      )}
+
+      {players.includes(playerObject.day_algo_address.addr.toString()) ? (
+        <p className="py-4 text-green-500">You have successfully joined the game!</p>
+      ) : (
+        <button className="btn mt-4" onClick={handleJoinGame}>
+          Join Game
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default JoinGameLobby
